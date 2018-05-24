54ce2348f82af9eb16ff42004b312f554b2c5a56
delemojw <delemojw@rose-hulman.edu>
2018-05-24 13:36:45 -0400

    Attempted Problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Jabari-Aman Delemore.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1a()
+    run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -89,6 +89,12 @@ def problem1a(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    s = 0
+    print(numbers)
+    while len(numbers) % 2 == 0:
+        if len(numbers) >= 3:
+            s = numbers[len(numbers) - 1] + numbers[len(numbers)/2] + numbers[0]
+    return s
     # -------------------------------------------------------------------------
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
@@ -165,6 +171,8 @@ def problem1b(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    
+
     # -------------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
